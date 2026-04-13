e357c1ede28caa484030d1a24a97ca09365d5c1e
David <David.Nabel-Moris@etu.univ-smb.fr>
2026-04-13 15:31:53 +0200

    mdrrrr

---
+++ Main.py
@@ -0,0 +1,16 @@
+print("1. Une procédure partition alternative du tri rapide")
+
+
+
+def assert_check(cond, message):
+    if not cond:
+        raise AssertionError(f"[VIOLATION] {message}")
+
+def precondition_partition(t, p, q):
+    return 0 <= p <= q < len(t)
+
+
+print(assert_check(False,"shhhhhhhhhhhit"))
+
+
+def 
